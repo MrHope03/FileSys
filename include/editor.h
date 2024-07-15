0bc56d22bff2e1589a2b034fbf0fc8b8656d692f
MrHope03 <nitishsgk@gmail.com>
2024-07-16 00:04:06 +0530

    Buffer is made resizable in editor

--- include/editor.h
+++ include/editor.h
@@ -2,20 +2,31 @@
 #define EDITOR_H
 
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
-#include <stdbool.h>
 
 #include "sysfn.h"
 
+typedef struct {
+  char *data;
+  size_t length;
+  size_t capacity;
+} Buffer;
+
+void buffer_init(Buffer *buffer);
+void buffer_append(Buffer *buffer, char c);
+void buffer_insert(Buffer *buffer, size_t pos, char c);
+void buffer_delete(Buffer *buffer, size_t pos);
+void buffer_free(Buffer *buffer);
+
 void disable_raw_mode();
 void enable_raw_mode();
-void read_file(char *filename, char *content);
-void write_file(char *filename, char *content);
-void display_buffer(char *file, char *buffer, bool isSaved);
+void read_file(char *filename, Buffer *buffer);
+void write_file(char *filename, Buffer *buffer);
+void display_buffer(char *file, Buffer *buffer, bool isSaved);
 void editor(char *nodeName);
-
 #endif
--- src/editor.c
+++ src/editor.c
@@ -1,9 +1,57 @@
 #include "../include/editor.h"
 
-#define MAX_LENGTH 1000
+#include <stdlib.h>
+
+#define INITIAL_CAPACITY 1000
+#define GROWTH_FACTOR 2
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 struct termios orig_termios;
+
+void buffer_init(Buffer *buffer) {
+  buffer->data = malloc(INITIAL_CAPACITY);
+  if (buffer->data == NULL) {
+    perror("Failed to allocate memory");
+    exit(1);
+  }
+  buffer->length = 0;
+  buffer->capacity = INITIAL_CAPACITY;
+}
+
+void buffer_append(Buffer *buffer, char c) {
+  if (buffer->length + 1 >= buffer->capacity) {
+    size_t new_capacity = buffer->capacity * GROWTH_FACTOR;
+    char *new_data = realloc(buffer->data, new_capacity);
+    if (new_data == NULL) {
+      perror("Failed to reallocate memory");
+      exit(1);
+    }
+    buffer->data = new_data;
+    buffer->capacity = new_capacity;
+  }
+  buffer->data[buffer->length++] = c;
+  buffer->data[buffer->length] = '\0';
+}
+
+void buffer_insert(Buffer *buffer, size_t pos, char c) {
+  buffer_append(buffer, '\0');  // This ensures we have space
+  memmove(&buffer->data[pos + 1], &buffer->data[pos], buffer->length - pos);
+  buffer->data[pos] = c;
+}
+
+void buffer_delete(Buffer *buffer, size_t pos) {
+  if (pos < buffer->length) {
+    memmove(&buffer->data[pos], &buffer->data[pos + 1], buffer->length - pos);
+    buffer->length--;
+  }
+}
+
+void buffer_free(Buffer *buffer) {
+  free(buffer->data);
+  buffer->data = NULL;
+  buffer->length = 0;
+  buffer->capacity = 0;
+}
 
 void disable_raw_mode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
@@ -20,30 +68,31 @@ void enable_raw_mode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-void read_file(char *filename, char *content) {
+void read_file(char *filename, Buffer *buffer) {
   FILE *file = fopen(filename, "r");
   if (file == NULL) {
     printf("Error opening file. Starting with empty buffer.\n");
-    content[0] = '\0';
     return;
   }
-  fread(content, 1, MAX_LENGTH - 1, file);
-  content[MAX_LENGTH - 1] = '\0';
+  int c;
+  while ((c = fgetc(file)) != EOF) {
+    buffer_append(buffer, c);
+  }
   fclose(file);
 }
 
-void write_file(char *filename, char *content) {
+void write_file(char *filename, Buffer *buffer) {
   FILE *file = fopen(filename, "w");
   if (file == NULL) {
     printf("Error opening file for writing.\n");
     return;
   }
-  fputs(content, file);
+  fwrite(buffer->data, 1, buffer->length, file);
   fclose(file);
   printf("\r\nFile saved.\r\n");
 }
 
-void display_buffer(char *file, char *buffer, bool isSaved) {
+void display_buffer(char *file, Buffer *buffer, bool isSaved) {
   write(STDOUT_FILENO, "\x1b[2J", 4);  // Clear the screen
   write(STDOUT_FILENO, "\x1b[H", 3);   // Move cursor to home position
 
@@ -51,52 +100,50 @@ void display_buffer(char *file, char *buffer, bool isSaved) {
   if (isSaved) flag = ' ';
   printf("Press Ctrl-S to save, Ctrl-Q to exit.\r\nEditing file %s%c\r\n\r\n", file, flag);
 
-  printf("%s", buffer);
+  printf("%s", buffer->data);
   fflush(stdout);
 }
 
 void editor(char *fileName) {
-  char buffer[MAX_LENGTH] = {0};
-  int buf_pos = 0;
+  Buffer buffer;
+  buffer_init(&buffer);
+  size_t cursor_pos = 0;
   int c;
 
-  read_file(fileName, buffer);
-  buf_pos = strlen(buffer);
+  read_file(fileName, &buffer);
+  cursor_pos = buffer.length;
 
   enable_raw_mode();
-  display_buffer(fileName, buffer, true);
+  display_buffer(fileName, &buffer, true);
 
   while (1) {
     c = getchar();
     if (c == CTRL_KEY('s')) {
-      write_file(fileName, buffer);
-      display_buffer(fileName, buffer, true);
+      write_file(fileName, &buffer);
+      display_buffer(fileName, &buffer, true);
     } else if (c == CTRL_KEY('q')) {
       printf("\r\nExiting...\r\n");
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       break;
     } else if (c == 127) {  // Backspace
-      if (buf_pos > 0) {
-        buf_pos--;
-        memmove(&buffer[buf_pos], &buffer[buf_pos + 1], strlen(buffer) - buf_pos);
-        display_buffer(fileName, buffer, false);
+      if (cursor_pos > 0) {
+        cursor_pos--;
+        buffer_delete(&buffer, cursor_pos);
+        display_buffer(fileName, &buffer, false);
       }
     } else if (c == 13) {  // Enter key
-      if (buf_pos < MAX_LENGTH - 2) {
-        memmove(&buffer[buf_pos + 1], &buffer[buf_pos], strlen(buffer) - buf_pos + 1);
-        buffer[buf_pos++] = '\r';
-        buffer[buf_pos++] = '\n';
-        display_buffer(fileName, buffer, false);
-      }
+      buffer_insert(&buffer, cursor_pos, '\r');
+      cursor_pos++;
+      buffer_insert(&buffer, cursor_pos, '\n');
+      cursor_pos++;
+      display_buffer(fileName, &buffer, false);
     } else if (isprint(c) || c == '\t') {
-      if (buf_pos < MAX_LENGTH - 1) {
-        memmove(&buffer[buf_pos + 1], &buffer[buf_pos], strlen(buffer) - buf_pos + 1);
-        buffer[buf_pos++] = c;
-        display_buffer(fileName, buffer, false);
-        (buffer);
-      }
+      buffer_insert(&buffer, cursor_pos, c);
+      cursor_pos++;
+      display_buffer(fileName, &buffer, false);
     }
   }
+  buffer_free(&buffer);
   disable_raw_mode();
 }
